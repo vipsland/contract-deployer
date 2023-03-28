de4406cd88f325c2b430108fb7d2767879ec528d
Anja Ishmukhametova <miukki@gmail.com>
2023-03-28 23:27:35 +1100

    proof for airdrop

---
+++ scripts/allowlist_internal.js
@@ -0,0 +1,12 @@
+/**
+ *  This array contains the addresses of the whitelisted users.
+ *  Make sure to add your white-listed users to this array. 
+ *
+ *  if you add a new user address to the whitelist or remove an existing user address from the whitelist,
+ *  you must change the merkleroot in the contract. For this reason, I created a new script to update the merkleroot
+ *  in the contract. You can find it in `scripts/setMerkleRoot.js`.
+ */
+
+module.exports = [
+  '0x555506C1583600a5148652c673E551E31848B329',
+]
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,8 @@
 const hre = require('hardhat')
 const { MerkleTree } = require('merkletreejs')
 const keccak256 = require('keccak256')
-const whitelist = require('./whitelist.js')
+const allowlist_airdrop = require('./allowlist_airdrop.js')
+const allowlist_internal = require('./allowlist_internal.js')
 
 async function main() {
 
@@ -28,9 +29,13 @@ async function main() {
   const _notRevealedUri = "https://ipfs.vipsland.com/nft/collections/genesis/json/hidden.json";
   const _revealedUri = "https://ipfs.vipsland.com/nft/collections/genesis/json/";
 
-  const leafNodes = whitelist.map((addr) => keccak256(addr))
-  const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true })
-  const root = merkleTree.getRoot()
+  const leafNodes_air = allowlist_airdrop.map((addr) => keccak256(addr))
+  const merkleTree_air = new MerkleTree(leafNodes_air, keccak256, { sortPairs: true })
+  const root_air = merkleTree_air.getRoot()
+
+  const leafNodes_int = allowlist_internal.map((addr) => keccak256(addr))
+  const merkleTree_int = new MerkleTree(leafNodes_int, keccak256, { sortPairs: true })
+  const root_int = merkleTree_int.getRoot()
 
 
   // Deploy the contract
@@ -43,7 +48,8 @@ async function main() {
     _teamShares,
     _notRevealedUri,
     _revealedUri,
-    root
+    root_air,
+    root_int
   )
 
   await vipslandContract.deployed()
